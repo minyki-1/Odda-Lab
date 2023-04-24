30e1d65f2716d239539b439ba0ab0cfada17dde3
CWIN77 <cwstbp0426@gmail.com>
2023-04-24 21:27:59 +0900

    모바일에서 드래그엔드롭기능 추가

--- src/pages/play/[id].tsx
+++ src/pages/play/[id].tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
-// import SVG_plus from "../../svg/plus.svg"
-// import SVG_down from "../../svg/down.svg"
+import SVG_plus from "../../svg/plus.svg"
+import SVG_down from "../../svg/down.svg"
 import { Dispatch, EventHandler, MouseEventHandler, SetStateAction, useEffect, useState } from 'react'
 
 const temp = {
@@ -40,17 +40,63 @@ export default function Play() {
   const [leftObj, setLeftObj] = useState<string>()
   const [rightObj, setRightObj] = useState<string>()
   const data = temp;
-  const [touch, setTouch] = useState<string>("byll")
+
+  const handlePointerDown = ({ clientX, clientY, target }: { clientX: number, clientY: number, target: EventTarget }) => {
+    setObjId((target as HTMLElement).id)
+    setX(String(clientX - 65 / 2))
+    setY(String(clientY - 65 / 2))
+  }
+  const handlePointerMove = (e: MouseEvent) => {
+    setX(String(e.clientX - 65 / 2))
+    setY(String(e.clientY - 65 / 2))
+  }
+  const leftCombine = ({ target }: { target: EventTarget }) => {
+    setLeftObj(objId);
+    (target as HTMLElement).style.backgroundImage = "url(" + data.objects.find(value => value.id === objId)?.img + ")"
+  }
+  const rightCombine = ({ target }: { target: EventTarget }) => {
+    setLeftObj(objId);
+    (target as HTMLElement).style.backgroundImage = "url(" + data.objects.find(value => value.id === objId)?.img + ")"
+  }
+
+  const makeDragDrop = () => {
+    if (typeof navigator !== "object") return
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+      return {
+        move: {
+          onPointerMove: (e: any) => {
+            setLeftObj(String(e.touches[0].clientX - 65 / 2))
+            setRightObj(String(e.touches[0].clientY - 65 / 2))
+          }
+        },
+        left: { onTouchEnd: leftCombine },
+        right: { onTouchEnd: rightCombine },
+        click: {
+          onTouchStart: (e: any) => {
+            setObjId((e.target as HTMLElement).id)
+            setLeftObj(String(e.touches[0].clientX - 65 / 2))
+            setRightObj(String(e.touches[0].clientY - 65 / 2))
+          }
+        }
+      }
+    } else {
+      return {
+        move: { onPointerMove: handlePointerMove },
+        left: { onPointerUp: leftCombine },
+        right: { onPointerUp: rightCombine },
+        click: { onPointerDown: handlePointerDown }
+      }
+    }
+  }
 
   useEffect(() => {
 
   }, [leftObj, rightObj])
 
   return (
-    <Container id="playCont" onTouchMove={(e: any) => { console.log(e) }} onClick={() => objId ? setObjId(undefined) : null}>
-      <div style={{ width: 100, height: 100, backgroundColor: "red" }} onTouchStart={() => { setTouch("123454323456532") }}></div>
-      <h1>{touch}</h1>
-      {/* {
+    <Container id="playCont" {...makeDragDrop()?.move} onClick={() => objId ? setObjId(undefined) : null}>
+      {
         objId && <GrapObj style={{
           left: x + "px",
           top: y + "px",
@@ -63,26 +109,25 @@ export default function Play() {
           <Title>
             <h1>{data.title}</h1>
           </Title>
-          <Combine>
-            <div onPointerUp={leftCombine} />
+          <CombineWrap>
+            <Combine {...makeDragDrop()?.left} />
             <Plus />
-            <div onPointerUp={leftCombine} />
-          </Combine>
+            <Combine {...makeDragDrop()?.right} />
+          </CombineWrap>
           <ObjCont>
-            <ObjectBox>
-              <ObjectHead>
+            <ObjectBackground>
+              <ObjectHeader>
                 <h1>오브젝트</h1>
                 <DownBtn />
-              </ObjectHead>
+              </ObjectHeader>
               <ObjectList>
                 {
                   data.objects.filter(value => data.found.includes(value.id))
                     .map((value, key) => (
                       <Object key={key}>
-                        <div
+                        <ObjectImg
                           id={value.id}
-                          // onPointerDown={handleClick}
-                          onPointerDown={handleClick}
+                          {...makeDragDrop()?.click}
                           style={{ backgroundImage: "url(" + value.img + ")" }}
                         />
                         <h1>{value.name}</h1>
@@ -90,11 +135,11 @@ export default function Play() {
                     ))
                 }
               </ObjectList>
-            </ObjectBox>
+            </ObjectBackground>
           </ObjCont>
         </Contents>
         <Ads>Ads</Ads>
-      </Main> */}
+      </Main>
     </Container>
   )
 }
@@ -105,177 +150,179 @@ const Container = styled.div`
   display:flex;
   flex-direction: column;
 `
-// const Header = styled.header`
-//   width:100vw;
-//   height:48px;
-//   background-color: beige;
-// `
-// const Main = styled.div`
-//   flex:1;
-//   display:flex;
-//   @media screen and (max-width: 800px) {
-//     flex-direction: column;
-//   }
-// `
-// const Contents = styled.main`
-//   flex:1;
-//   background: linear-gradient(180deg, rgba(65, 65, 65, 0.75) 0%, rgba(65, 65, 65, 0) 37.62%), url(/image.jpg);
-//   background-position: center center;
-//   background-repeat: repeat-x;
-//   background-size: cover;
-//   display:flex;
-//   flex-direction: column;
-//   justify-content: space-around;
-// `
-// const Title = styled.div`
-//   width:100%;
-//   display:flex;
-//   justify-content: center;
-//   align-items: center;
-//   h1{
-//     font-size: 40px;
-//     font-weight: bold;
-//     color:white;
-//     @media screen and (max-width: 800px) {
-//       font-size: 32px;
-//     }
-//   }
-// `
-// const Combine = styled.div`
-//   width:100%;
-//   display:flex;
-//   align-items: center;
-//   justify-content: center;
-//   div{
-//     width:140px;
-//     height:140px;
-//     margin: 0px 24px;
-//     border-radius: 1000px;
-//     background: rgba(255, 255, 255, 0.3);
-//     border: 3px solid rgba(255, 255, 255, 0.6);
-//     backdrop-filter: blur(3px);
-//     background-position: center center;
-//     background-repeat: repeat-x;
-//     background-size: cover;
-//     @media screen and (max-width: 800px) {
-//       width:100px;
-//       height:100px;
-//       border: 2px solid rgba(255, 255, 255, 0.6);
-//       margin: 0px 12px;
-//     }
-//   }
-// `
-// const ObjCont = styled.div`
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-// `
-// const ObjectBox = styled.div`
-//   background: rgba(0, 0, 0, 0.2);
-//   border: 3px solid rgba(255, 255, 255, 0.2);
-//   backdrop-filter: blur(3.5px);
-//   border-radius: 12px;
-//   padding-bottom: 24px;
-//   @media screen and (max-width: 800px) {
-//     flex-direction: column;
-//     padding-bottom: 12px;
-//     border: 2px solid rgba(255, 255, 255, 0.2);
-//   }
-// `
-// const Plus = styled(SVG_plus)`
-//   width:65px;
-//   height:65px; 
-//   fill:white;
-//   @media screen and (max-width: 800px) {
-//     width:45px;
-//     height:45px; 
-//   }
-// `
-// const Ads = styled.div`
-//   width:200px;
-//   background-color: #d6d6d6;
-//   @media screen and (max-width: 800px) {
-//     flex-direction: column;
-//     height:100px;
-//     width:100vw;
-//   }
-// `
-// const ObjectHead = styled.div`
-//   display:flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   padding:24px;
-//   padding-top: 18px;
-//   * {
-//     color:white;
-//   }
-//   @media screen and (max-width: 800px) {
-//     padding: 16px;
-//     padding-top: 12px;
-//     h1{
-//       font-size: 15px;
-//     }
-//   }
-// `
-// const ObjectList = styled.div`
-//   display:flex;
-//   padding: 0px 16px;
-//   @media screen and (max-width: 800px) {
-//     padding: 0px 6px;
-//   }
-// `
-// const Object = styled.div`
-//   display:flex;
-//   flex-direction: column;
-//   align-items: center;
-//   justify-content: center;
-//   margin: 0px 16px;
-//   div{
-//     width:65px;
-//     height:65px;
-//     cursor: grab;
-//     border-radius: 100px;
-//     background-color: white;
-//     background-position: center center;
-//     background-repeat: repeat-x;
-//     background-size: cover
-//   }
-//   h1{
-//     margin-top: 12px;
-//     font-size: 16px;
-//     color:white;
-//   }
-//   @media screen and (max-width: 800px) {
-//     div{
-//       width:45px;
-//       height:45px;
-//     }
-//     h1{
-//       margin-top: 8px;
-//       font-size: 14px;
-//       color:white;
-//     }
-//   }
-// `
-// const DownBtn = styled(SVG_down)`
-//   width:28px;
-//   height:28px; 
-//   fill:white;
-//   @media screen and (max-width: 800px) {
-//     width:20px;
-//     height:20px; 
-//   }
-// `
-// const GrapObj = styled.div<{ x: string, y: string }>`
-//   width:65px;
-//   height:65px;
-//   cursor:grabbing;
-//   position: absolute;
-//   z-index: 100;
-//   border-radius: 100px;
-//   background-color: white;
-//   background-position: center center;
-//   background-repeat: repeat-x;
-//   background-size: cover;
-//   pointer-events:none;
-// `
+const Header = styled.header`
+  width:100vw;
+  height:48px;
+  background-color: beige;
+`
+const Main = styled.div`
+  flex:1;
+  display:flex;
+  @media screen and (max-width: 800px) {
+    flex-direction: column;
+  }
+`
+const Contents = styled.main`
+  flex:1;
+  background: linear-gradient(180deg, rgba(65, 65, 65, 0.75) 0%, rgba(65, 65, 65, 0) 37.62%), url(/image.jpg);
+  background-position: center center;
+  background-repeat: repeat-x;
+  background-size: cover;
+  display:flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
+const Title = styled.div`
+  width:100%;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  h1{
+    font-size: 40px;
+    font-weight: bold;
+    color:white;
+    @media screen and (max-width: 800px) {
+      font-size: 32px;
+    }
+  }
+`
+const CombineWrap = styled.div`
+  width:100%;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+`
+const Combine = styled.div`
+  width:140px;
+  height:140px;
+  margin: 0px 24px;
+  border-radius: 1000px;
+  background: rgba(255, 255, 255, 0.3);
+  border: 3px solid rgba(255, 255, 255, 0.6);
+  backdrop-filter: blur(3px);
+  background-position: center center;
+  background-repeat: repeat-x;
+  background-size: cover;
+  @media screen and (max-width: 800px) {
+    width:100px;
+    height:100px;
+    border: 2px solid rgba(255, 255, 255, 0.6);
+    margin: 0px 12px;
+  }
+`
+const ObjCont = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const ObjectBackground = styled.div`
+  background: rgba(0, 0, 0, 0.2);
+  border: 3px solid rgba(255, 255, 255, 0.2);
+  backdrop-filter: blur(3.5px);
+  border-radius: 12px;
+  padding-bottom: 24px;
+  @media screen and (max-width: 800px) {
+    flex-direction: column;
+    padding-bottom: 12px;
+    border: 2px solid rgba(255, 255, 255, 0.2);
+  }
+`
+const Plus = styled(SVG_plus)`
+  width:65px;
+  height:65px; 
+  fill:white;
+  @media screen and (max-width: 800px) {
+    width:45px;
+    height:45px; 
+  }
+`
+const Ads = styled.div`
+  width:200px;
+  background-color: #d6d6d6;
+  @media screen and (max-width: 800px) {
+    flex-direction: column;
+    height:100px;
+    width:100vw;
+  }
+`
+const ObjectHeader = styled.div`
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  padding:24px;
+  padding-top: 18px;
+  * {
+    color:white;
+  }
+  @media screen and (max-width: 800px) {
+    padding: 16px;
+    padding-top: 12px;
+    h1{
+      font-size: 15px;
+    }
+  }
+`
+const ObjectList = styled.div`
+  display:flex;
+  padding: 0px 16px;
+  @media screen and (max-width: 800px) {
+    padding: 0px 6px;
+  }
+`
+const Object = styled.div`
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 16px;
+  h1{
+    margin-top: 12px;
+    font-size: 16px;
+    color:white;
+  }
+  @media screen and (max-width: 800px) {
+    h1{
+      margin-top: 8px;
+      font-size: 14px;
+      color:white;
+    }
+  }
+`
+const ObjectImg = styled.div`
+  width:65px;
+  height:65px;
+  cursor: grab;
+  border-radius: 100px;
+  background-color: white;
+  background-position: center center;
+  background-repeat: repeat-x;
+  background-size: cover;
+  @media screen and (max-width: 800px) {
+    div{
+      width:45px;
+      height:45px;
+    }
+  }
+`
+const DownBtn = styled(SVG_down)`
+  width:28px;
+  height:28px; 
+  fill:white;
+  @media screen and (max-width: 800px) {
+    width:20px;
+    height:20px; 
+  }
+`
+const GrapObj = styled.div<{ x: string, y: string }>`
+  width:65px;
+  height:65px;
+  cursor:grabbing;
+  position: absolute;
+  z-index: 100;
+  border-radius: 100px;
+  background-color: white;
+  background-position: center center;
+  background-repeat: repeat-x;
+  background-size: cover;
+  pointer-events:none;
+`
