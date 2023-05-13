8306b092307d07e96479ffe0bdde6b270484a9d0
CWIN77 <cwstbp0426@gmail.com>
2023-05-13 22:05:11 +0900

    create 페이지 변경

--- src/pages/create/[id].tsx
+++ src/pages/create/[id].tsx
@@ -193,6 +193,7 @@ export default function Create() {
                 style={{ display: "none" }}
               />
             </MainInput>
+
             {/* TODO: 오디오 파일 받기 */}
           </MainInputWrap>
         </Contents>
