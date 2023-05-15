95debc922683873147e1ed2243b03f0d7ba99d54
CWIN77 <cwstbp0426@gmail.com>
2023-05-15 18:51:19 +0900

    메인 페이지 정리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,66 +2,9 @@ import styled from 'styled-components'
 
 
 export default function Home() {
-  function typedArrayToURL(typedArray: any, mimeType: any) {
-    return URL.createObjectURL(new Blob([typedArray.buffer], { type: mimeType }))
-  }
-  const handleClickBtn = () => {
-
-    return new Promise(function (resolve, reject) {
-      try {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "~~~~~");
-        xhr.responseType = "blob";
-        xhr.onerror = function () { reject("Network error.") };
-        xhr.onload = function () {
-          if (xhr.status === 200) { resolve(xhr.response) }
-          else { reject("Loading error:" + xhr.statusText) }
-        };
-        xhr.send();
-      }
-      catch (err) { reject(err) }
-    });
-  }
-  function previewImage(event: any) {
-    const file = event.target.files[0];
-
-    if (!file.type.match("image.*")) {
-      alert("이미지 파일만 선택해주세요.");
-      return;
-    }
-
-    const reader = new FileReader();
-
-    reader.onload = (function (theFile) {
-      return function (e) {
-        const url: any = e.target?.result;
-        return new Promise(function (resolve, reject) {
-          try {
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", url);
-            xhr.responseType = "blob";
-            xhr.onerror = function () { reject("Network error.") };
-            xhr.onload = function () {
-              if (xhr.status === 200) {
-                resolve(xhr.response);
-                const url = window.URL.createObjectURL(xhr.response);
-                console.log(url)
-              }
-              else { reject("Loading error:" + xhr.statusText) }
-            };
-            xhr.send();
-          }
-          catch (err) { reject(err) }
-        });
-      };
-    })(file);
-
-    reader.readAsDataURL(file);
-  }
   return (
     <Container>
-      <input type='file' onChange={previewImage} />
-      <h1>My Next WebApp</h1>
+
     </Container>
   )
 }
