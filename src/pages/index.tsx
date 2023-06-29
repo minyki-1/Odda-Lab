e6e3650814098eab7974747d337711c04124560b
CWIN77 <cwstbp0426@gmail.com>
2023-06-29 21:25:55 +0900

    메인페이지 db 연결, 디자인  변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import SVG_search from "../svg/search.svg"
 import Link from "next/link"
 import SVG_heart_fill from "../svg/heart-fill.svg"
 import NextImage from 'next/image'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 interface IObj {
   id: string,
@@ -24,35 +24,67 @@ interface ILabList {
 
 export default function Home({ data }: { data: ILabList[] }) {
   const [labList, setLabList] = useState(data)
+  const labListRef = useRef(null);
+
+  const styling = (comp: HTMLElement, style: Object) => {
+    for (const [key, value] of Object.entries(style)) comp.style[key as any] = value;
+  }
+
+  const handleScroll = () => {
+    const labListComp = labListRef.current as HTMLElement | null;
+    labListComp?.childNodes.forEach((node, key) => {
+      const comp = node as HTMLElement
+      const playButton = comp.getElementsByClassName(comp.id + "playBtn")[0] as HTMLElement
+      const rect = (comp as HTMLElement).getBoundingClientRect();
+      const onStyle = { width: "70px", height: "35px", backgroundColor: "rgba(245, 245, 245, 1)", color: "rgba(20,20,20,1)" }
+      const offStyle = { width: "20px", height: "20px", backgroundColor: "rgba(245, 245, 245, 0.5)", color: "rgba(20,20,20,0)" }
+      if (rect.top > 80 && rect.top < 350) {
+        playButton.innerText = "Play";
+        styling(playButton, onStyle)
+      } else {
+        playButton.innerText = "";
+        styling(playButton, offStyle)
+      }
+    });
+  }
 
   useEffect(() => {
-    console.log(labList)
-  }, [labList])
+    if (typeof navigator !== "object") return;
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+      handleScroll()
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, [])
 
   return (
     <Container>
       <Header></Header>
       <SortWrapper>
-        <div>
+        <TitleWrap>
           <SortTitle select={"true"}>Popular</SortTitle>
           <SortTitle >Newest</SortTitle>
           <SortTitle >Workspace</SortTitle>
-        </div>
+        </TitleWrap>
         <Search>
           <input type="text" />
-          <SVG_search fill="#F1F6F9" width={24} height={24} />
+          <SVG_search fill="#F1F6F9" />
         </Search>
       </SortWrapper>
-      <LabList>
+      <LabList ref={labListRef}>
         {
           labList.map((data, key) => (
-            <Lab href={`/play/${data.id}`} key={key}>
+            <Lab key={key} id={data.id}>
+              <BgGradient />
               <NextImage
                 fill={true}
                 src={data.background_img}
                 alt={'BackgroundImage'}
               />
-              
               <LabInfo>
                 <h1>{data.title}</h1>
                 <div>
@@ -62,6 +94,7 @@ export default function Home({ data }: { data: ILabList[] }) {
                   <h2>{data.maker_name}</h2>
                 </div>
               </LabInfo>
+              <LabPlayBtn href={`/play/${data.id}`} className={data.id + "playBtn"}>Play</LabPlayBtn>
               <PreviewObjList>
                 {
                   data.start_obj.slice(0, 3).map((data, key) => (
@@ -111,15 +144,24 @@ const SortWrapper = styled.div`
   display:flex;
   justify-content: space-between;
   align-items: center;
-  padding: 24px 40px;
-  div{
-    display: flex;
+  padding: 16px 30px;
+  @media screen and (max-width: 800px) {
+    justify-content: start;
+    align-items: start;
+    flex-direction: column;
+    padding: 12px 18px;
   }
 `
+const TitleWrap = styled.div`
+  display: flex;
+  align-items: center;
+  height:45px;
+  margin-bottom: 12px;
+`
 const SortTitle = styled.h1<{ select: string }>`
-  font-size: 20px;
+  font-size: 16px;
   padding: 2px;
-  margin-right:18px;
+  margin-right:16px;
   cursor: pointer;
   opacity: ${({ select }: { select: string }) => select === "true" ? 1 : 0.6};
   &:hover{
@@ -132,34 +174,98 @@ const Search = styled.div`
   align-items: center;
   border-radius: 4px;
   input{
-    width:360px;
-    font-size: 16.5px;
+    width:250px;
+    font-size: 16px;
     padding:12px 16px;
   }
   svg{
+    width:22px;
+    height:22px;
     padding:12px 16px;
     cursor: pointer;
+  }
+  @media screen and (max-width: 800px) {
+    width:100%;
+    input{
+      flex:1;
+      font-size: 16px;
+      padding:12px 16px;
+    }
+    svg{
+      width:20px;
+      height:20px;
+      padding:10px 14px;
+      cursor: pointer;
+    }
   }
 `
 const LabList = styled.div`
   display:flex;
   flex-wrap: wrap;
   flex: 1;
+  padding-bottom: 300px;
 `
-const Lab = styled(Link)`
+const Lab = styled.div`
   display:flex;
   justify-content: space-between;
   flex-direction: column;
+  align-items: flex-end;
   margin-left: 30px;
   margin-top: 20px;
   margin-bottom: 20px;
-  background-color: #252B30;
-  width:calc(50% - 45px);
-  aspect-ratio: 16 / 9;
+  width:calc(100% / 2 - 45px);
+  aspect-ratio: 16 / 10;
+  background-color: white;
   overflow: hidden;
   border-radius: 8px;
   box-shadow: 0px 0px 12px 6px rgba(0,0,0,0.2);
   position: relative;
+  img{
+    object-fit: cover;
+    z-index: 1;
+  }
+  @media screen and (min-width: 1500px) {
+    width:calc(100% / 3 - 40px);
+  }
+  @media screen and (max-width: 800px) {
+    width:calc(100% - 40px);
+    margin-left: 20px;
+    aspect-ratio: 16 / 10;
+  }
+`
+const LabPlayBtn = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 3;
+  margin: 24px 28px;
+  width:30px;
+  height:30px;
+  border-radius: 8px;
+  border: none;
+  background-color: rgba(245, 245, 245, 0.5);
+  transition: 0.25s;
+  box-shadow: 0px 0px 16px 4px rgba(0,0,0,0.2);
+  color:rgba(20,20,20,0);
+  cursor: pointer;
+  font-size: 18px;
+  &:hover{
+    width:90px;
+    height:45px;
+    background-color: rgba(245, 245, 245, 1);
+    color:rgba(20,20,20,1);
+  }
+  @media screen and (max-width: 500px) {
+    font-size: 16px;
+    margin: 16px 20px;
+    width:20px;
+    height:20px;
+    border-radius: 6px;
+    &:hover{
+      width:70px;
+      height:35px;
+    }
+  }
 `
 const PreviewObjList = styled.div`
   display:flex;
@@ -178,36 +284,63 @@ const PreviewObj = styled.div`
   border-radius: 100px;
   overflow: hidden;
   margin: 20px;
+  z-index: 3;
   position: relative;
   @media screen and (max-width: 800px) {
-    width:50px;
-    height:50px;
-  }
-  img{
-    object-fit: cover;
+    width:15%;
+    margin: 14px;
   }
 `
 const LabInfo = styled.div`
-  padding: 22px;
+  padding: 22px 24px;
+  width:calc(100% - 48px);
   display:flex;
   align-items: center;
   justify-content: space-between;
-  z-index: 1;
+  z-index: 3;
   div{
     display:flex;
     align-items: center;
   }
   h1{
     font-size: 18px;
-    margin-right: 20px;
   }
   h2{
     font-size: 16px;
     margin-left: 8px;
-    margin-right: 18px;
+    margin-right: 4px;
   }
   img{
-    margin-left: 6px;
+    margin-left: 16px;
     border-radius: 100px;
   }
+  @media screen and (max-width: 500px) {
+    padding: 16px 20px;
+    width:calc(100% - 40px);
+    h1{
+      font-size: 16px;
+    }
+    h2{
+      font-size: 13px;
+      margin-left: 6px;
+      margin-right: 2px;
+    }
+    img{
+      width:20px;
+      height:20px;
+      margin-left: 14px;
+      border-radius: 100px;
+    }
+    svg{
+      width:18px;
+      height:18px;
+    }
+  }
+`
+const BgGradient = styled.div`
+  width:100%;
+  height:100%;
+  position: absolute;
+  z-index: 2;
+  background: linear-gradient(0deg, rgba(0,0,0,0) 70%, rgba(0,0,0,0.5) 100%); 
 `
